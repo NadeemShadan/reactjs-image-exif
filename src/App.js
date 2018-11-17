c42d9df86be653d11919915b760d911df09ec5c8
Nadeem Shadan <nadeem.s@iinerds.com>
2018-11-17 18:11:14 +0530

    Added basic image efix reading,preparing webcam integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
+import Input from '@material-ui/core/Input';
+// import TextField from '@material-ui/core/TextField';
+import * as loadImage from 'blueimp-load-image'
 
+
+class ImgReader extends Component {
+  workWorkWork(e) 
+  {
+    let files=e.target.files;
+    loadImage.parseMetaData(files[0], (data) => {
+      if(data.exif){
+        console.log('data: ', data.exif.getAll());
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <Input type="file" onChange={(e)=>this.workWorkWork(e)}/>
+        </div>
+        <Webcam audio={false} />
+      </div>
+    )
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>EXIF DATA READER</h1>
+          <ImgReader />
       </div>
     );
   }
